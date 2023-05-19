255af96973d469c694978dcb65bcfc5f4c4ac620
Eligio Biasi <ex_98@live.it>
2023-05-19 17:56:57 +0200

    solved snack 2

---
+++ jsnack-2/script.js
@@ -0,0 +1,17 @@
+
+// Generare numero casuale tra 1 e 100 COMPRESI
+const randomNumber = Math.floor(Math.random() * 100) + 1;
+console.log(randomNumber);
+let playerNumber = parseInt(prompt('indovina un numero da 1 a 100'));
+
+while(playerNumber!=randomNumber){
+    if(playerNumber>randomNumber){
+        console.log('the pc number is lower')
+        playerNumber = parseInt(prompt('riprova'));
+    }else{
+        console.log('the pc number is higher')
+        playerNumber = parseInt(prompt('riprova'))
+    }
+} 
+console.log('your guess is right')
+alert('hai indovinato');
