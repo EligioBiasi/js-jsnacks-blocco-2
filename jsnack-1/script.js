5dab9d425a1ee2cc3b4d79c619d30b3860df6083
Eligio Biasi <ex_98@live.it>
2023-05-19 16:33:38 +0200

    init

---
+++ jsnack-1/script.js
@@ -0,0 +1,10 @@
+// Crea un array vuoto
+const emptyBox = []
+
+// chiedi all'utente un numero da inserire nell'array.
+let fifty = 0
+
+while(fifty<=50){
+    let numberAdded= parseInt(prompt('inserisci un numero'));
+}
+// Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50
