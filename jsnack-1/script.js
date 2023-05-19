70d9ed243cb47bb489e4f0797d9ce2bb08f37d8b
Eligio Biasi <ex_98@live.it>
2023-05-19 16:56:06 +0200

    partial snack solution

--- jsnack-1/script.js
+++ jsnack-1/script.js
@@ -2,9 +2,11 @@
 const emptyBox = []
 
 // chiedi all'utente un numero da inserire nell'array.
-let fifty = 0
+let fifty = 0;
 
-while(fifty<=50){
-    let numberAdded= parseInt(prompt('inserisci un numero'));
+while(fifty<50){
+    let numberAdded = parseInt(prompt('inserisci un numero'));
+    fifty+=numberAdded
+    emptyBox.push(numberAdded);
 }
-// Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50
+console.log(emptyBox)
