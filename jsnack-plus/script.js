a51100665ec4434a08fdb71fbd795bfa41b26693
Eligio Biasi <ex_98@live.it>
2023-05-23 12:42:47 +0200

    added li

--- jsnack-plus/script.js
+++ jsnack-plus/script.js
@@ -1,3 +1,13 @@
-function item (tag,tagClass){
+
+const ulElement = document.querySelector('ul');
+
+for(let i = 0; i<10; i++){
+    ulElement.appendChild(newItem());
     
+}
+
+function newItem(){
+    const Item = document.createElement('li');
+    Item.innerHTML='è un`oggetto'
+    return Item
 }
