a49c6fe29fbe5cd3a77ff0a3e234a094550ecdf3
Eligio Biasi <ex_98@live.it>
2023-05-29 12:04:21 +0200

    attempt

---
+++ jsnack-peperone/script.js
@@ -0,0 +1,68 @@
+
+// Crea un array di 10 oggetti che rappresentano un peperone, indicando per ognuno varietà, peso e lunghezza.
+const pepperContainer=[
+    pepper1={
+        varietà:'pepe',
+        peso:randomNum(100, 0),
+        lunghezza: randomNum(100, 0),
+    },
+    pepper2={
+        varietà:'pepe',
+        peso:randomNum(100, 0),
+        lunghezza: randomNum(100, 0),
+    },
+    pepper3={
+        varietà:'pepe',
+        peso:randomNum(100, 0),
+        lunghezza: randomNum(100, 0),
+    },
+    pepper4={
+        varietà:'pepe',
+        peso:randomNum(100, 0),
+        lunghezza: randomNum(100, 0),
+    },
+    pepper5={
+        varietà:'pepe',
+        peso:randomNum(100, 0),
+        lunghezza: randomNum(100, 0),
+    },
+    pepper6={
+        varietà:'pepe',
+        peso:randomNum(100, 0),
+        lunghezza: randomNum(100, 0),
+    },
+    pepper7={
+        varietà:'pepe',
+        peso:randomNum(100, 0),
+        lunghezza: randomNum(100, 0),
+    },
+    pepper8={
+        varietà:'pepe',
+        peso:randomNum(100, 0),
+        lunghezza: randomNum(100, 0),
+    },
+    pepper9={
+        varietà:'pepe',
+        peso:randomNum(100, 0),
+        lunghezza: randomNum(100, 0),
+    },
+    pepper10={
+        varietà:'pepe',
+        peso:randomNum(100, 0),
+        lunghezza: randomNum(100, 0),
+    },
+]
+// Calcola quanto pesano tutti i peperoni.
+
+const pepperContainer=[
+    for(let chiave in pepperContainer){
+        
+    }
+
+
+
+
+function randomNum(maxNum, minNum) {
+    let Num = (Math.floor(Math.random() * maxNum)+minNum);
+    return Num
+}
